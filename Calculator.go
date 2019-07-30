185e8dc5d120e37455ad09e19b66b802d0b4dfd0
aks369 <36281503+aks369@users.noreply.github.com>
2019-07-30 19:55:04 +0530

    fix operator validation

--- Calculator.go
+++ Calculator.go
@@ -3,30 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	var num1, num2 float64
+	var num1, num2 int
 	var op string
 
 	fmt.Println("Which operation do you want to perform? (+, -, *, /, %)")
 	fmt.Scan(&op)
 
-	fmt.Println("Enter the two numbers: ")
-	fmt.Scan(&num1, &num2)
+	if op == "+" || op == "-" || op == "*" || op == "/" || op == "%" {
+		fmt.Println("Enter the two numbers: ")
+		fmt.Scan(&num1, &num2)
 
-	switch op {
-	case "+":
-		fmt.Println(num1 + num2)
-		break
-	case "-":
-		fmt.Println(num1 - num2)
-		break
-	case "*":
-		fmt.Println(num1 * num2)
-		break
-	case "/":
-		fmt.Println(num1 / num2)
-		break
-	default:
-		fmt.Println("wrong operation")
-
+		switch op {
+		case "+":
+			fmt.Println("Sum = ", num1+num2)
+		case "-":
+			fmt.Println("Differece = ", num1-num2)
+		case "*":
+			fmt.Println("Product = ", num1*num2)
+		case "/":
+			fmt.Println("Quotient = ", num1/num2)
+		case "%":
+			fmt.Println("Remainder = ", num1%num2)
+		default:
+			fmt.Println("invalid")
+		}
+	} else {
+		fmt.Println("Invalid Operation")
 	}
+
 }
